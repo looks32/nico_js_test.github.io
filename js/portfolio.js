8b69a611682ea4ada97a86a15afe422fb0d71e69
Dang <56210432+looks32@users.noreply.github.com>
2024-12-15 19:02:38 +0900

    12.15

--- js/portfolio.js
+++ js/portfolio.js
@@ -51,44 +51,28 @@ document.body.style.backgroundImage = `url('img/${chosenImage}')`;
 // 명언
 const quotes = [
 	{
-		quote : '준비하지 않은 자는 기회가 와도 소용없다.',
-		author : '알렉시스 드 토크빌'
+		quote : '왜 쳐다봐 강해rrr륀',
+		author : '팜하니'
 	},
 	{
-		quote : '노력해라 집착하라. 숙명적인 노력을.',
-		author : '레오나르도 다 빈치'
+		quote : '돈까쓰요.',
+		author : '해린'
 	},
 	{
-		quote : '내일이란 오늘의 다른 이름일 뿐',
-		author : '윌리엄 포크너'
+		quote : '오늘 나이키 오픈스토어 준비 갈 완료',
+		author : '팜하니'
 	},
 	{
-		quote : '강인한 의지 없이는 뛰어난 재능도 없다.',
-		author : '오노레 드 발자크'
+		quote : '준비하셔야되요. 이런거는 뜬겁새로 뜬급',
+		author : '팜하니'
 	},
 	{
-		quote : '나는 날마다 모든 면에서 점점 좋아지고 있다.',
-		author : '에밀쿠에'
+		quote : '역시 탄수나물.',
+		author : '팜하니'
 	},
 	{
-		quote : '불가능한 일을 해보는 것은 신나는 일이다.',
-		author : '월트 디즈니'
-	},
-	{
-		quote : '할 수 있다고 믿는 사람은 결국 그렇게 된다.',
-		author : '샤론 드골'
-	},
-	{
-		quote : '당신이 포기할 때, 나는 시작한다.',
-		author : '엘론 머스크'
-	},
-	{
-		quote : '나는 이룰 때까지 노력할 것이다.',
-		author : '브라이언 트레이시'
-	},
-	{
-		quote : '한번 포기하면 습관이 된다. 절대 포기하지 말아라.',
-		author : '마이클 조던'
+		quote : '킴민지 또 날 디스해!',
+		author : '팜하니'
 	}
 ]
 
@@ -103,7 +87,6 @@ quotesWrap.appendChild(authorP)
 
 quoteP.innerText = todaysQuote.quote;
 authorP.innerText = todaysQuote.author;
-
 
 
 // 날씨
